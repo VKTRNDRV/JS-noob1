c2d012b1ed4922f678827c757aeb85305dff134f
Viktor Androv <117111938+VKTRNDRV@users.noreply.github.com>
2022-12-05 21:07:36 +0200

    Add files via upload

--- mountainRun.js
+++ mountainRun.js
@@ -1,16 +1,16 @@
 function mountainRun(input){
 
-    let recordTimeScnds = Number(input[0]);
+    let recordTimeSeconds = Number(input[0]);
     let distanceMeters = Number(input[1]);
     let secondsPerMeter = Number(input[2]);
     let slowdownSeconds = (Math.floor(distanceMeters/50))*30;
 
     let attemptTimeSeconds = (distanceMeters * secondsPerMeter) + slowdownSeconds;
 
-    if(attemptTimeSeconds < recordTimeScnds){
+    if(attemptTimeSeconds < recordTimeSeconds){
         console.log(`Yes! The new record is ${attemptTimeSeconds.toFixed(2)} seconds.`)
     }else{
-        console.log(`No! He was ${(attemptTimeSeconds - recordTimeScnds).toFixed(2)} seconds slower.`);
+        console.log(`No! He was ${(attemptTimeSeconds - recordTimeSeconds).toFixed(2)} seconds slower.`);
     }
 }
 
